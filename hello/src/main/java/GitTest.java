4112a92adce0f2c59f3bf285b43416b73f7d9823
admin <iskgdm@163.com>
2020-08-03 14:58:15 +0800

    添加了hello的工程文件

---
+++ hello/src/main/java/GitTest.java
@@ -0,0 +1,22 @@
+/**
+ * @Author: SK
+ * @Date: 2020/8/3 14:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("文件成功推送到github");
+        int i=1;
+        int num=0;
+        if(i<=100){
+            num+=i;
+            i++;
+        }
+        System.out.println("num="+num);
+        System.out.println("####################################");
+
+        int a=33;
+        int b=0;
+        b+=a;
+        System.out.println("b="+b);
+    }
+}
